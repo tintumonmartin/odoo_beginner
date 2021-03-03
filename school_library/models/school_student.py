fae83d3e0e8911a95efb6e1580ad8fd41408661a
Tintumon <tintumonmartin@gmail.com>
2021-03-03 09:44:48 +0530

    Search & Search read

--- school_library/models/school_student.py
+++ school_library/models/school_student.py
@@ -9,7 +9,7 @@ class SchoolStudent(models.Model):
     _description = 'School Student'
     _rec_name = 'student_identity'
 
-    name = fields.Char("Student Name", required=True)
+    name = fields.Char(string="Student Name", required=True)
     active = fields.Boolean('Active', default=True)
     student_identity = fields.Char(readonly=True)
     user_id = fields.Many2one('res.users', string='Assigned to')
@@ -98,13 +98,17 @@ class SchoolStudent(models.Model):
         print(self)
         school_student = self.env['school.student'].search([])
         print(school_student)
-
+        school_student = self.search([])
+        print(school_student)
         print(len(school_student))
         # for student in school_student:
         #     print(student)
         #     print(student.result)
-        school_student = self.env['school.student'].search([('result', '=', 'pass')])
+        school_student = self.env['school.student'].search([('result', '=', 'pass')], limit=1, order='id desc')
         print("________________")
         print(school_student)
         print(len(school_student))
+        school_student = self.env['school.student'].search_read(
+            [('result', '=', 'pass')], fields=['name', 'gender', 'result'], limit=1, order='id asc')
+        print(school_student)
         print("school_student")
